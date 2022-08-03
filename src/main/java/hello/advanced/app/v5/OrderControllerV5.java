5b7bf5b7332bf624f5ba7a112c4604de96b890cf
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-03 13:41:31 +0900

    Feat : 콜백 패턴 추가

---
+++ src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate template;
+
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace trace) {
+        this.orderService = orderService;
+        this.template = new TraceTemplate(trace);
+    }
+
+    @GetMapping("/v5/request")
+    public ResponseEntity<String> request(@RequestParam String itemId) {
+        template.execute("OrderController.request()", () -> "OK");
+        return ResponseEntity.ok().build() ;
+    }
+}
---
+++ src/test/java/hello/advanced/app/v5/OrderControllerV5Test.java
@@ -0,0 +1,67 @@
+package hello.advanced.app.v5;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OrderControllerV5Test {
+    @LocalServerPort
+    private int port;
+    private final String URI = "/v4/request";
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void request() {
+        ExtractableResponse<Response> 주문_요청 = RestAssured.given().log().all()
+            .queryParam("itemId", "hello")
+            .when().get(URI)
+            .then().log().all().extract();
+
+        assertThat(주문_요청.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    void exceptionTest() {
+        ExtractableResponse<Response> 주문_요청 = RestAssured.given().log().all()
+            .queryParam("itemId", "ex")
+            .when().get(URI)
+            .then().log().all().extract();
+
+        assertThat(주문_요청.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @Test
+    void manyRequest() {
+
+        ExtractableResponse<Response> 주문_요청1 = RestAssured.given().log().all()
+            .queryParam("itemId", "hello")
+            .when().get(URI)
+            .then().log().all().extract();
+
+        ExtractableResponse<Response> 예외_발생_요청 = RestAssured.given().log().all()
+            .queryParam("itemId", "ex")
+            .when().get(URI)
+            .then().log().all().extract();
+
+        ExtractableResponse<Response> 주문_요청3 = RestAssured.given().log().all()
+            .queryParam("itemId", "hello")
+            .when().get(URI)
+            .then().log().all().extract();
+
+        assertThat(주문_요청1.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(예외_발생_요청.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(주문_요청3.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+}
